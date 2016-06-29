f35503bb0bae069cae8d35e5a80a9951a3c7e33a
Ladex <ladipoilori@yahoo.com>
2016-06-29 22:18:52 +0100

    Adding hash to filename

--- app/component.js
+++ app/component.js
@@ -2,6 +2,6 @@ var styles = require('./main.css');
 module.exports = function() {
     var element = document.createElement('h1');
     element.className = styles.redButton;
-    element.innerHTML = 'Hello this is the new world!';
+    element.innerHTML = 'Hello this is the new world!!!';
     return element;
 };
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,14 @@ var config;
 // Detect how npm is run and branch based on that
 switch (process.env.npm_lifecycle_event) {
     case 'build':
-        config = merge(common, { devtool: 'source-map' },
+        config = merge(common, {
+                devtool: 'source-map',
+                output:{
+                    path: PATHS.build,
+                    filename: '[name].[chunkhash].js',
+                    chunkFilename:'[chunkhash].js'
+                }
+            },
             parts.setFreeVariable('process.env.NODE_ENV', 'production'),
             parts.minify(),
             parts.setupCSS(PATHS.app),
